8a842e8003c219e074b116840ed58ed50e79513c
Thomas <thomashu.fr@gmail.com>
2019-03-20 12:26:37 +0800

    Add_First_Timing_table

--- main_benoit_v1.py
+++ main_benoit_v1.py
@@ -1,6 +1,6 @@
-### IMPORTATING STATION ### 
+### IMPORTATING STATION ###
 from instruction_queue import *
-from reservation_station import * 
+from reservation_station import *
 from load_station import *
 from register import Registers
 
@@ -11,12 +11,11 @@ import numpy as np
 import os
 
 
-
 ### PARAMETERS ###
 memory_file_name = "memory.txt"
 input_file_name = "input2.txt"
 
-nb_add =3
+nb_add = 3
 nb_mult = 2
 nb_load = 6
 nb_store = 6
@@ -35,52 +34,55 @@ max_iter = 100
 
 val_reg = np.zeros(nb_register)
 
-reg_init = [6.0,2,3.5,4,10.0,6,7.8,8,9]
+reg_init = [6.0, 2, 3.5, 4, 10.0, 6, 7.8, 8, 9]
 
 for i in range(len(reg_init)):
-    val_reg[i]=reg_init[i]
+    val_reg[i] = reg_init[i]
 
-    
-    
-### MAIN FUNCTION ###
+clock = 0
+pc = 0
 
-def main():
-    global pc
-    global main_clock
-    RESVNUMCONFIG = {
+RESVNUMCONFIG = {
     'Add': nb_add,
     'Mult': nb_mult,
     'Load': nb_load,
     'Store': nb_store
-    }
+}
 
-    Add = Add_RS(RESVNUMCONFIG)
-    Mult = Mul_RS(RESVNUMCONFIG)
-    Load = Load_Station(RESVNUMCONFIG, memory_file_name)
-    Store = Store_Station(RESVNUMCONFIG, memory_file_name)
-    Register = Registers(nb_register,val_reg )
+Add = Add_RS(RESVNUMCONFIG)
+Mult = Mul_RS(RESVNUMCONFIG)
+Load = Load_Station(RESVNUMCONFIG, memory_file_name)
+Store = Store_Station(RESVNUMCONFIG, memory_file_name)
+Register = Registers(nb_register, val_reg)
+timing_table = Timing()
 
-    pc = 0
-    main_clock = 1
-    
+### MAIN FUNCTION ###
+
+
+def main():
+    global pc
+    global clock
+
     for i in range(max_iter):
         phase_one()
     return
+
 
 def phase_one():
     instruction = instructions[pc]
     regs = instruction.replace(",", " ").split(" ")
 
-### SIDE FUNCTIONS ### 
+### SIDE FUNCTIONS ###
+
 
 def extract_offset_reg(instruction_text):
-    inst_split = instruction_text.regs[2].replace(')','(').split('(')
+    inst_split = instruction_text.regs[2].replace(')', '(').split('(')
     offset = inst_split[0]
     print("offset : ", offset)
     reg_value = inst_split[1][1]
     print("reg_value : ", reg_value)
-    return (offset,reg_value)
-    
+    return (offset, reg_value)
+
 
 def input_file_decoder(in_file):
     input_file = open(in_file, 'r')
@@ -92,16 +94,24 @@ def input_file_decoder(in_file):
     return instruction_buffer
 
 
+def instruction_table(instructions):
+    for i in range(len(instructions)):
+        instruction = instructions[i].replace(",", " ")
+        timing_table.timing_table_add(i, instruction)
+
+
 if __name__ == '__main__':
     input("Press Enter to Start")
-    print("Input_file : "+ input_file_name)
-    print("Memory_file : "+ memory_file_name)
+    print("Input_file : " + input_file_name)
+    print("Memory_file : " + memory_file_name)
     if len(input_file_name) > 1:
-        print ("Importing "+ input_file_name)
+        print("Importing " + input_file_name)
         instructions = input_file_decoder(input_file_name)
+        instruction_table(instructions)
+        print("Clock cycle :", clock)
+        print ("\n") 
+        timing_table.printList()
         main()
     else:
-        print ("Please specify input file!")
+        print("Please specify input file!")
         exit(1)
-        
-        
